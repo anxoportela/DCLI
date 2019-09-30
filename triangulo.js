8877e583c1b774712f011e262cd207ec9cdf09b6
Anxo Portela-Insua Blanco <anxoportela@gmail.com>
2019-09-30 20:30:38 +0200

    Update

---
+++ triangulo.js
@@ -0,0 +1,8 @@
+var base = prompt('Introduzca la base del triángulo:', 0);
+var altura = prompt('Introduzca la altura del triángulo:', 0);
+
+if (isNaN(base) || isNaN(altura)) {
+    alert('Introduzca valores numéricos');
+} else {
+    alert((base * altura) / 2);
+};
