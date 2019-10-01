47604d41e88cbd4edebd9ff82d69bf41b7ac77f3
Anxo Portela-insua Blanco <anxoportela@gmail.com>
2019-10-01 02:47:43 +0200

    Update

--- precioiva.js
+++ precioiva.js
@@ -3,9 +3,11 @@ var precio = prompt('Introduzca el precio del artículo:', 0);
 var iva = prompt('Introduzca el iva del artículo:', 0);
 
 if (isNaN(precio) || isNaN(iva)) {
-    alert('Introduzca valores numéricos');
-} else if (iva != 0) {
-    alert('El precio del producto ' + nombre + 'es: ' + precio*iva)
+  alert('Introduzca valores numéricos');
+} else if (iva=="") {
+  let precioiva=precio*1.21;
+  alert('El precio del producto ' + nombre + ' es: ' + precioiva + ' euros');
 } else {
-    alert('El precio del producto ' + nombre + 'es: ' + precio*21% );
+  let precioiva=precio*(1+iva/100);
+  alert('El precio del producto ' + nombre + ' es: ' + precioiva + ' euros');
 }
