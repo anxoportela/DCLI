9cdd083d799d1ecc417f3668143c655b1353ab97
Anxo Portela-Insua Blanco <anxoportela@gmail.com>
2019-11-26 18:39:10 +0100

    Update

---
+++ Unidad_4/LanzaDados/lanzaDados.js
@@ -0,0 +1,68 @@
+var cuentaUno = 0;
+var cuentaDos = 0;
+var cuentaTres = 0;
+var cuentaCuatro = 0;
+var cuentaCinco = 0;
+var cuentaSeis = 0;
+var miVentana = null;
+
+
+function lanzarDado() {
+    var numero = Math.round(Math.random() * 6 + 1);
+    crearVentana();
+    switch (numero) {
+        case 1:
+            miVentana.document.getElementById("img").src = "img/1.png";
+            cuentaUno++;
+            modPorcentaje();
+            miVentana.focus();
+            break;
+        case 2:
+            miVentana.document.getElementById("img").src = "img/2.png";
+            cuentaDos++;
+            modPorcentaje();
+            miVentana.focus();
+            break;
+        case 3:
+            miVentana.document.getElementById("img").src = "img/3.png";
+            cuentaTres++;
+            modPorcentaje();
+            miVentana.focus();
+            break;
+        case 4:
+            miVentana.document.getElementById("img").src = "img/4.png";
+            cuentaCuatro++;
+            modPorcentaje();
+            miVentana.focus();
+            break;
+        case 5:
+            miVentana.document.getElementById("img").src = "img/5.png";
+            cuentaCinco++;
+            modPorcentaje();
+            miVentana.focus();
+            break;
+        case 6:
+            miVentana.document.getElementById("img").src = "img/6.png";
+            cuentaSeis++;
+            modPorcentaje();
+            miVentana.focus();
+            break;
+    }
+
+}
+
+function modPorcentaje() {
+    document.getElementById("porcentaje").innerHTML = "<p>Cantidad de unos: " + cuentaUno + "<br>Cantidad de dos: " + cuentaDos + "<br>Cantidad de tres: " + cuentaTres + "<br>Cantidad de cuatros: " + cuentaCuatro + "<br>Cantidad de cincos: " + cuentaCinco + "<br>Cantidad de seis: " + cuentaSeis + "</p>";
+}
+
+function crearVentana() {
+    if (miVentana == null || miVentana.closed) {
+        miVentana = window.open("", "", "height=200,width=100");
+        miVentana.document.write('<img src="" id="img"></img><br><button type="button" onclick="window.opener.cerrarVentana();">Cerrar</button>');
+    }
+
+}
+
+function cerrarVentana() {
+    miVentana.close();
+}
