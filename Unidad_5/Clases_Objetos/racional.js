9d80d9352089ada23aa5f973ac66c208c34992af
unknown <dual108@ACARBALLEIRA.LOCAL>
2020-01-21 21:41:55 +0100

    Update

--- Unidad_5/Clases_Objetos/racional.js
+++ Unidad_5/Clases_Objetos/racional.js
@@ -1,23 +1,23 @@
 class numeroRacional {
     constructor(Numerador, Denominador) {
-        this._numerador = Numerador;
-        this._denominador = Denominador;
+        this.Num = Numerador;
+        this.Den = Denominador;
     }
 
     get Numerador() {
-        return this._numerador;
+        return this.Num;
     }
 
     set Numerador(Numerador) {
-        this._numerador = Numerador;
+        this.Num = Numerador;
     }
 
     get Denominador() {
-        return this._denominador;
+        return this.Den;
     }
 
     set Denominador(Denominador) {
-        this._denominador = Denominador;
+        this.Den = Denominador;
     }
 }
 
@@ -36,10 +36,55 @@ function simplificar(x, y) {
             y /= i;
         }
     }
-    
+
     console.log(x + "/" + y);
 }
 
-var nuevoRacional = new numeroRacional(2, 4);
-console.log(nuevoRacional.Numerador+"/"+nuevoRacional.Denominador);
-simplificar(nuevoRacional.Numerador, nuevoRacional.Denominador);
+function sumarfraccionario(f1n, f1d, f2n, f2d) {
+    var den = f1d * f2d;
+    var num = f1n * f2d + f2n * f1d;
+    simplificar(num, den);
+}
+
+function restafraccionario(f1n, f1d, f2n, f2d) {
+    var den = f1d * f2d;
+    var num = f1n * f2d - f2n * f1d;
+    simplificar(num, den);
+}
+
+function multiplicafraccionario(f1n, f1d, f2n, f2d) {
+    var num = f1n * f2n;
+    var den = f1d * f2d;
+    simplificar(num, den);
+}
+
+function dividefraccionario(f1n, f1d, f2n, f2d) {
+    var num = f1n * f2d;
+    var den = f2n * f1d;
+    simplificar(num, den);
+}
+
+function creararray() {
+    var x = parseInt(prompt("Introduzca el tamaño del array"));
+    var arr = new Array(x);
+    for(var i=0;i<x;i++){
+        arr[i] = pedirfracciones();
+    }
+    console.log(arr);
+}
+
+function pedirfracciones() {
+    var num = prompt("Introduzca un numerador");
+    var den = prompt("Introduzca un denominador");
+    var frac = new numeroRacional(num,den);
+    return frac;
+}
+
+//var fr1 = new numeroRacional(1, 2);
+//var fr2 = new numeroRacional(1, 2);
+
+//sumarfraccionario(fr1.Num,fr1.Den,fr2.Num,fr2.Den);
+//restafraccionario(fr1.Num,fr1.Den,fr2.Num,fr2.Den);
+//multiplicafraccionario(fr1.Num,fr1.Den,fr2.Num,fr2.Den);
+//dividefraccionario(fr1.Num, fr1.Den, fr2.Num, fr2.Den);
+creararray();
